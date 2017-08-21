55f2200e7a49c4ec553562bcaf6352d57cf27338
wuxx <wujianhua0924@gmail.com>
2017-08-21 21:40:53 +0800

    update

---
+++ convert.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#convert png to video
+
+dirname=$1
+
+files=$(ls *.png)
+
+i=0
+
+for file in $files
+do
+    ls $file
+    echo $i
+    i=$(($i+1));
+    cp $file a$i.png
+done
+
+#ffmpeg -f image2 -framerate 12 -i 20170821/a%d.png test.gif
+#ffmpeg -framerate 30 -i 20170821/a%d.png -codec copy output.mkv
